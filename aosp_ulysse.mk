7ba0b9e105b4d3544aadaf46d593d55ff534e49e
laleeroy <gabwielcastwo@gmail.com>
2022-02-01 01:17:45 +0800

    Arcana Bringup
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- aosp_ulysse.mk
+++ aosp_ulysse.mk
@@ -9,9 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit some common ArcaneOS stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 # Inherit from ulysse device
 $(call inherit-product, device/xiaomi/ulysse/device.mk)
 
@@ -20,7 +19,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ulysse
-PRODUCT_NAME := lineage_ulysse
+PRODUCT_NAME := aosp_ulysse
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A / Y1
